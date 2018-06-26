497b795cac2b8e33c7caa2da2b0bde785e871d96
Sombrio <rodrigo.sombrio@gmail.com>
2018-06-25 22:41:24 -0300

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var nodecache = require("node-cache");
+var localcache = new nodecache();
+
 var local = false;
 
 var mytest = {
@@ -5,11 +8,17 @@ var mytest = {
   set: function(value) {
     this.logical = value;
     local = value;
+    localcache.set("local", local);
     console.log('in set', this.logical, local);
   },
   get: function() {
-    console.log('in get', this.logical, local);
-    return this.logical;
+    v = localcache.get("local");
+    console.log('in get', this.logical, local, v);
+    if (v) {
+      return v;
+    } else {
+      return this.logical;
+    }
   }
 
 }
